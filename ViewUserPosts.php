796382a8ad0c90ce40964ef3e30b0625366b929b
okaysierra <scseacat@gmail.com>
2016-03-21 21:27:31 -0500

    Create ViewUserPosts

---
+++ ViewUserPosts.php
@@ -0,0 +1,23 @@
+<?php
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "sseacat", "vZFhQCymmaDqpWvs", "sseacat");
+
+  if ($mysqli->connect_errno) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+  }
+
+  $user = $_POST["users"];
+
+  $query = "SELECT content FROM Posts WHERE user_id=". $user ."";
+
+
+  if($result = $mysqli->query($query)){
+    while ($row = $result->fetch_assoc()) {
+          printf ("%s (%s)\n", $row["Posts"]);
+      }
+      $result->free();
+  }
+
+  $mysqli->close();
+
+?>
