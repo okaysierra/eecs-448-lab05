b048a72d5a3a5c17b38acd182a1304411c1ee7a4
okaysierra <scseacat@gmail.com>
2016-03-22 22:09:09 -0500

    Get DeletePost working

--- DeletePost.php
+++ DeletePost.php
@@ -1,16 +1,21 @@
 <?php
-mysqli = new mysqli("mysql.eecs.ku.edu", "sseacat", "vZFhQCymmaDqpWvs", "sseacat");
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "sseacat", "vZFhQCymmaDqpWvs", "sseacat");
 
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
 
-$deletes = $_POST["selectposts"];
 
-echo $deletes;
-
-$query = "DELETE FROM Posts WHERE post_id = " . $deletes . "";
+echo "Posts being deleted<br>";
+if(!empty($_POST["selectposts"])){
+foreach($_POST["selectposts"] as $deletes){
+  echo "$deletes<br>";
+  $query = "DELETE FROM Posts WHERE post_id ='$deletes'";
+  $mysqli->query($query);
+}
+}
 
 $mysqli->close();
 
